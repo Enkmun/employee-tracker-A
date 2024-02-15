ffdd1a29e7009cf33846462f3875684fc570c8e5
Enkmun <147563607+Enkmun@users.noreply.github.com>
2024-02-15 18:22:01 -0500

    removed personal pw

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,10 @@
 const mysql = require('mysql2');
-//require('dotenv').config();
-
 const db = mysql.createConnection({
     host: 'localhost',
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'Enkhnaran1995!',
+    password: '',
     database: 'employee_tracker_db'
 });
 
